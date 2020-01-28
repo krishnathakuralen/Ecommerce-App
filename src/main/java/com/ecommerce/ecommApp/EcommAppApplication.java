47f6a81b0be15acb84eecc149b91e143cefd11e8
fatema-sagar <fatema.sagar@hashedin.com>
2020-01-28 14:54:50 +0530

    Initial Commit

---
+++ src/main/java/com/ecommerce/ecommApp/EcommAppApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/ecommApp/EcommAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

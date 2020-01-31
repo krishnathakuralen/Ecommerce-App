bb1bdc0cb7942e72c31a70161be1db13c45a3fed
krishna <krishna.thakur@hashedin.com>
2020-01-31 16:09:39 +0530

    models

--- src/main/java/com/ecommerce/ecommApp/products/models/Cart.java
+++ src/main/java/com/ecommerce/ecommApp/products/models/Cart.java
@@ -1,7 +1,31 @@
 package com.ecommerce.ecommApp.products.models;
+import lombok.Getter;
+import lombok.Setter;
 
-import javax.persistence.Table;
 
+
+import javax.persistence.*;
+@Getter
+@Setter
 @Table
 public class Cart {
+
+    private Long ProductID;
+
+    private Long CustomerID;
+
+    @Id
+    @Column(name = "CartID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long CartID;
+
+
+    @Override
+    public String toString() {
+        return "Cart{" +
+                "ProductID=" + ProductID +
+                ", CustomerID=" + CustomerID +
+                ", CartID=" + CartID +
+                '}';
+    }
 }
--- src/main/java/com/ecommerce/ecommApp/products/models/Inventory.java
+++ src/main/java/com/ecommerce/ecommApp/products/models/Inventory.java
@@ -1,7 +1,38 @@
 package com.ecommerce.ecommApp.products.models;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Table;
 
-@Table
+import javax.persistence.*;
+
+@Entity
+@Table(name = "inventory")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Inventory {
+
+    @Id
+    @Column(name = "InventoryID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long InventoryID;
+
+    @Column(name = "availabilityCount")
+    private int available;
+
+    @Column(name = "size")
+    private String size;
+
+    @Column(name = "cost")
+    private int cost;
+
+    @Override
+    public String toString() {
+        return "Inventory{" +
+                "available=" + available +
+                ", size='" + size + '\'' +
+                ", cost=" + cost +
+                '}';
+    }
 }
